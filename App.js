369793b739358eba2262b855b6bb8fc62cbcc4b8
Talha Hisham <talhahisham92@gmail.com>
2023-01-18 21:37:44 -0600

    Prayers Connect Code

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.containerText}>Home Screen</Text>
+      <Button
+        title="Go to Second Screen"
+        onPress={() => navigation.navigate('Second')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+function SecondScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.containerText}>Second Screen</Text>
+      <Button
+        title="Go to Home Screen"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Second' component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aliceblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerText: {
+    color: 'green',
+    fontSize: 30
+  }
 });
